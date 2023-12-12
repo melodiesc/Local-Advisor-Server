66460f6965d0ab8eebd3053b9a135160aa1bcecb
KevinRMO <romanokevin1992@gmail.com>
2023-12-12 16:25:37 +0100

    Beta connexion user/owner

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Validation\ValidationException;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+use App\Models\Owner;
 
 class LoginController extends Controller
 {
@@ -27,27 +30,54 @@ class LoginController extends Controller
     /**
      * Store a newly created resource in storage.
      */
+    // public function store(Request $request)
+    // {
+    //     $credentials = $request->validate([
+    //         'email' => 'required|email',
+    //         'password' => 'required',
+    //     ]);
+
+    //     // Tentative de connexion avec les informations fournies
+    //     if (Auth::attempt($credentials)) {
+    //         $user = $request->user();
+    //         // Générez le token d'authentification pour l'utilisateur
+    //         $token = $user->createToken('authToken')->plainTextToken;
+    //         return response()->json(['token' => $token], 200);
+    //     }
+
+    //     // En cas d'échec de l'authentification, retournez une erreur
+    //     throw ValidationException::withMessages([
+    //         'email' => ['Les informations d\'identification fournies sont incorrectes.'],
+    //     ]);
+
+    // }
     public function store(Request $request)
-    {
-        $credentials = $request->validate([
-            'email' => 'required|email',
-            'password' => 'required',
-        ]);
+{
+    $credentials = $request->validate([
+        'email' => 'required|email',
+        'password' => 'required',
+    ]);
 
-        // Tentative de connexion avec les informations fournies
-        if (Auth::attempt($credentials)) {
-            $user = $request->user();
-            // Générez le token d'authentification pour l'utilisateur
-            $token = $user->createToken('authToken')->plainTextToken;
-            return response()->json(['token' => $token], 200);
-        }
+    $user = User::where('email', $credentials['email'])->first();
 
-        // En cas d'échec de l'authentification, retournez une erreur
-        throw ValidationException::withMessages([
-            'email' => ['Les informations d\'identification fournies sont incorrectes.'],
-        ]);
-
+    if (!$user) {
+        $user = Owner::where('email', $credentials['email'])->first();
     }
+    
+    if ($user && Hash::check($credentials['password'], $user->password)) {
+        // Utilisateur trouvé dans la table "users" ou "owners"
+        // Traitez en conséquence s'il est un utilisateur ou un propriétaire
+        // ...
+
+        // Générer le token d'authentification
+        $token = $user->createToken('authToken')->plainTextToken;
+        return response()->json(['token' => $token, 'user' => $user], 200);
+    }
+
+    throw ValidationException::withMessages([
+        'email' => ['Les informations d\'identification fournies sont incorrectes.'],
+    ]);
+}
 
     /**
      * Display the specified resource.
