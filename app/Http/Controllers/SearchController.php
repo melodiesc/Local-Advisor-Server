8fccf8e44e0779cd703beeb7dd28e96fbc354ee5
John-LP <jonathanleplat@hotmail.fr>
2023-12-12 17:06:24 +0100

    màj searchcontroller pour recheche globale sans filtre

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -4,18 +4,15 @@ namespace App\Http\Controllers;
 
 use App\Models\Category;
 use Illuminate\Http\Request;
-
 use App\Models\Location;
-use App\Models\Owner;
 
 class SearchController extends Controller
 {
-    public function search(Request $request, Category $category)
+    public function search(Request $request, $category)
     {
         $searchText = $request->input('searchText');
-
-        $result = Location::where('category_id', $category->id)
-            ->join('owners', 'locations.owner_id', '=', 'owners.id')
+    
+        $query = Location::join('owners', 'locations.owner_id', '=', 'owners.id')
             ->join('categories', 'locations.category_id', '=', 'categories.id')
             ->select(
                 'locations.*',
@@ -31,9 +28,18 @@ class SearchController extends Controller
                     ->orWhere('owners.lastname', 'like', "%$searchText%")
                     ->orWhere('owners.firstname', 'like', "%$searchText%")
                     ->orWhere('categories.category', 'like', "%$searchText%");
-            })
-            ->get();
-
+            });
+    
+        if ($category === 'all') {
+            $query->whereIn('locations.category_id', [1, 2, 3]);
+        } else {
+            $categoryId = (int)$category;
+            $query->where('locations.category_id', $categoryId);
+        }
+    
+        $result = $query->get();
+    
         return response()->json($result);
     }
+    
 }
