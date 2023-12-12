17a331626f1cbe9b3ebde74d738f0928d02d78c9
KevinRMO <romanokevin1992@gmail.com>
2023-12-12 13:51:58 +0100

    Connexion fonctionnelle pour les membres

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
+
+class LoginController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        // Tentative de connexion avec les informations fournies
+        if (Auth::attempt($credentials)) {
+            $user = $request->user();
+            // Générez le token d'authentification pour l'utilisateur
+            $token = $user->createToken('authToken')->plainTextToken;
+            return response()->json(['token' => $token], 200);
+        }
+
+        // En cas d'échec de l'authentification, retournez une erreur
+        throw ValidationException::withMessages([
+            'email' => ['Les informations d\'identification fournies sont incorrectes.'],
+        ]);
+
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -8,9 +8,11 @@ use App\Http\Controllers\RegisterController;
 use Laravel\Sanctum\Http\Controllers\AuthenticatedSessionController;
 use Laravel\Sanctum\Http\Controllers\RegisteredUserController;
 use App\Http\Controllers\SearchController;
+use App\Http\Controllers\LoginController;
 
 Route::post('/register_user', [RegisterController::class, 'store']);
 Route::post('/register_owner', [OwnerController::class, 'store']);
+Route::post('/login', [LoginController::class, 'store']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
@@ -22,5 +24,5 @@ Route::get('/locations', [LocationController::class, 'index']);
 //     Route::post('/register', [ProductController::class, 'store']);
 // });
 
-// Route::post('/login', [AuthenticatedSessionController::class, 'store']);
+
 // Route::post('/logout', [AuthenticatedSessionController::class, 'destroy']);
