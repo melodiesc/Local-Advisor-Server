32cfd60acb8fbfb0791ef50195414918727e44f3
floriane6027 <lunettafloriane24@gmail.com>
2023-12-15 14:28:22 +0100

    fusion tables

--- database/migrations/2023_12_14_084453_create_notices_table.php
+++ database/migrations/2023_12_14_084453_create_notices_table.php
@@ -4,19 +4,18 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCommentsTable extends Migration
+class CreateNoticeTable extends Migration
 {
     public function up()
     {
-        Schema::create('comments', function (Blueprint $table) {
+        Schema::create('notices', function (Blueprint $table) {
             $table->id();
             $table->integer('user_id');
             $table->integer('location_id');
-            $table->text('content');
-            $table->integer('owner_id')->nullable();;
+            $table->text('comment');
+            $table->integer('rate');
             $table->timestamps();
 
-            $table->foreign('owner_id')->references('id')->on('owner');
             $table->foreign('user_id')->references('id')->on('users');
             $table->foreign('location_id')->references('id')->on('locations');
         });
@@ -24,6 +23,6 @@ class CreateCommentsTable extends Migration
 
     public function down()
     {
-        Schema::dropIfExists('comments');
+        Schema::dropIfExists('notices');
     }
 }
