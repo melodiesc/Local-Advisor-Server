c7d37b4ed5d0d6d40217200a90a3450453c29b2a
John-LP <jonathanleplat@hotmail.fr>
2023-12-18 14:02:05 +0100

    ajout sécurité et vérification a la création d'un post

--- app/Http/Controllers/LocationController.php
+++ app/Http/Controllers/LocationController.php
@@ -29,25 +29,47 @@ class LocationController extends Controller
 
     public function store(Request $request)
     {
+        $request->validate([
+            'owner_id'=> 'required',
+            'name'=> 'required',
+            'address'=> 'required',
+            'zip_code'=> 'required|numeric',
+            'city'=> 'required',
+            'category_id'=> 'required',
+            'description'=> 'required',
+            'image_path' => 'required|image|mimes:jpeg,png,jpg,gif|max:2048',
+        ]);
+    
         $image = $request->file('image_path');
+    
+        $sizeInKb = $image->getSize() / 1024;
+        if ($sizeInKb > 2048) {
+            return response()->json(['error' => 'L\'image est trop volumineuse, elle ne doit pas dépasser 2 Mo.'], 400);
+        }
+    
         $originalName = $image->getClientOriginalName();
-        $extension =$image->getClientOriginalExtension();
+        $extension = $image->getClientOriginalExtension();
+        $allowedExtensions = ['jpg', 'jpeg', 'png', 'gif'];
+    
+        if (!in_array(strtolower($extension), $allowedExtensions)) {
+            return response()->json(['error' => 'Format d\'image non pris en charge. Utilisez des images au format : jpg, jpeg, png ou gif.'], 400);
+        }
+    
         $imageName = time() . '_' . pathinfo($originalName, PATHINFO_FILENAME) . '.' . $extension;
-        $image_path = $image->storeAs( 'images', $imageName,'public' );
-        $request->image_path->move(public_path('images'), $imageName);
+        $image_path = $image->storeAs('images', $imageName, 'public');
+        $image->move(public_path('images'), $imageName);
 
-        $location = new Location();
-        $location->id = $request->id;
-        $location->owner_id = $request->owner_id;
-        $location->name = $request->name;
-        $location->address = $request->address;
-        $location->zip_code = $request->zip_code;
-        $location->city = $request->city;
-        $location->category_id = $request->category_id;
-        $location->description = $request->description;
-        $location->image_path = $image_path;
-        $location->save();
 
+        $location = Location::create([
+            'owner_id'=> $request->owner_id,
+            'name'=> $request->name,
+            'address'=> $request->address,
+            'zip_code'=> $request->zip_code,
+            'city'=> $request->city,
+            'category_id'=> $request->category_id,
+            'description'=> $request->description,
+            'image_path' => $image_path,
+        ]);
 
 
         return response()->json($location, 201);
